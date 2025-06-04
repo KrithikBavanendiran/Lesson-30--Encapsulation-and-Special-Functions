9bd6f505ada8b64087e26bbf91e3b229b8c65395
KrithikBavanendiran <bvino1522@gmail.com>
2025-06-04 18:40:15 +0100

    Lesson 30

---
+++ Class string reverse.py
@@ -0,0 +1,10 @@
+class Reverse:
+    def string_reverse(self,b):
+       
+        a=str(input("Enter your own string: "))
+        b=a[::-1]   
+        print(b)
+
+obj_str= Reverse()
+
+obj_str.string_reverse(input)
